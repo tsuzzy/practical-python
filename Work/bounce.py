76c75bc9c3c86c4d4bfed9ef5c624cbbfccdca32
Ruixin Song <tsuzsrx@gmail.com>
2021-05-17 15:17:43 -0230

    change bounce

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+
+for hit in range(0,10):
+    hit += 1
+    height *= 0.6
+    print(hit, height)
---
+++ Work/sears.py
@@ -0,0 +1,22 @@
+import sys
+
+bill_thick = 0.11 * 0.001
+sears_height =442
+
+num_bills = 1
+day = 1
+money_i_have = float(sys.argv[1])
+
+while num_bills * bill_thick < sears_height:
+    print(day, num_bills, num_bills*bill_thick)
+    day += 1
+    num_bills = num_bills * 2
+
+print('Number of days:', day)
+print('Number of bills:', num_bills)
+print('Final height:', num_bills * bill_thick)
+
+if num_bills < money_i_have:
+    print("Do it!")
+else:
+    print("Go to find enough money pls")
