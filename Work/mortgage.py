4021a5d1d4809bedb64e1d92032149737343b848
Ruixin <tsuzsrx@gmail.com>
2021-05-22 17:05:03 -0230

    Exercise 1.7.1: payment calculated monthly

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -4,15 +4,16 @@
 
 principal = 500000
 rate_year = 0.05
+rate_month = rate_year/12
 payment_month = 2684.11
 payment_year = payment_month * 12
 payment_sum = 0;
-count = 0;
+months = 0;
 
 while principal > 0:
-    principal = principal * (1 + rate_year) - payment_year
-    payment_sum += payment_year
-    count += 1
+    principal = principal * (1 + rate_month) - payment_month
+    payment_sum += payment_month
+    months += 1
 
 print('Total paid', payment_sum)
-print('Pay how many years', count)
+print('Pay how many months', months)
