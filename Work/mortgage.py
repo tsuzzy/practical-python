e2e650db3f81cd3b99566d38a96dbe724e35fb82
Ruixin <tsuzsrx@gmail.com>
2021-05-22 18:30:55 -0230

    Exercise 1.10: payment table

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -14,13 +14,14 @@ extra_payment_end_month = 108
 extra_payment = 1000
 
 while principal > 0:
-    if (months>=extra_payment_start_month) & (months<extra_payment_end_month):
+    if (months>=extra_payment_start_month) & (months<=extra_payment_end_month):
         principal = principal * (1 + rate) - payment - extra_payment
         payment_sum += payment + extra_payment
     else:
         principal = principal * (1 + rate) - payment
         payment_sum += payment
     months += 1
+    print(months, round(payment_sum,2), round(principal,2))
 
-print('Total paid', payment_sum)
+print('Total paid', round(payment_sum,2))
 print('Pay how many months', months)
