b92727ecf7abfee627d7eca8cb363ba157d51b29
Ruixin <tsuzsrx@gmail.com>
2021-05-22 16:15:58 -0230

    Exercise 1.6: bug fixed

--- Work/sears.py
+++ Work/sears.py
@@ -1,22 +1,15 @@
-import sys
+# sears.py
 
-bill_thick = 0.11 * 0.001
-sears_height =442
+bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
+sears_height   = 442             # Height (meters)
+num_bills      = 1
+day            = 1
 
-num_bills = 1
-day = 1
-money_i_have = float(sys.argv[1])
-
-while num_bills * bill_thick < sears_height:
-    print(day, num_bills, num_bills*bill_thick)
-    day += 1
+while num_bills * bill_thickness < sears_height:
+    print(day, num_bills, num_bills * bill_thickness)
+    day = day + 1
     num_bills = num_bills * 2
 
-print('Number of days:', day)
-print('Number of bills:', num_bills)
-print('Final height:', num_bills * bill_thick)
-
-if num_bills <= money_i_have:
-    print("Do it!")
-else:
-    print("Go to find enough money pls")
+print('Number of days', day)
+print('Number of bills', num_bills)
+print('Final height', num_bills * bill_thickness)
