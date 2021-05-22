d7dbeb04dcb4d3f40aa0bc3f6edf5b51339d4438
Ruixin <tsuzsrx@gmail.com>
2021-05-22 17:00:56 -0230

    Exercise 1.7: payment calculated yearly

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,18 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000
+rate_year = 0.05
+payment_month = 2684.11
+payment_year = payment_month * 12
+payment_sum = 0;
+count = 0;
+
+while principal > 0:
+    principal = principal * (1 + rate_year) - payment_year
+    payment_sum += payment_year
+    count += 1
+
+print('Total paid', payment_sum)
+print('Pay how many years', count)
