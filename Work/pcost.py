6b3871bbd6220578c4f478baf1375f7bfd550569
Ruixin <tsuzsrx@gmail.com>
2021-05-23 18:32:12 -0230

    Exercise 1.31: catch exception

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,19 @@
 # pcost.py
 #
-# Exercise 1.30
+# Exercise 1.31
 def portfolio_cost(filename):
     with open(filename, 'rt') as f:
         header = next(f).split(',')
         cost = 0.0
         for line in f:
-            row = line.split(',')
-            shares = int(row[1])
-            price = float(row[2])
+            try:
+                row = line.split(',')
+                shares = int(row[1])
+                price = float(row[2])
+            except ValueError:
+                print("This line missing something, couldn't parse:", line)
             cost += shares * price
     return cost
 
-cost = portfolio_cost('Data/portfolio.csv')
+cost = portfolio_cost('Data/missing.csv')
 print('Total cost', cost)
