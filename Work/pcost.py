738db9b4c618d9e0993611c442adc3529b380b65
Ruixin <tsuzsrx@gmail.com>
2021-05-23 18:13:15 -0230

    Exercise 1.30: define a function to call files

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,16 @@
 # pcost.py
 #
-# Exercise 1.27
-f = open('Data/portfolio.csv', 'rt')
-header = next(f).split(',')
+# Exercise 1.30
+def portfolio_cost(filename):
+    with open(filename, 'rt') as f:
+        header = next(f).split(',')
+        cost = 0.0
+        for line in f:
+            row = line.split(',')
+            shares = int(row[1])
+            price = float(row[2])
+            cost += shares * price
+    return cost
 
-cost = 0.0
-for line in f:
-    row = line.split(',')
-    shares = int(row[1])
-    price = float(row[2])
-    cost += shares * price
-
+cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost', cost)
-
-f.close()
