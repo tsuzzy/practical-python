5c7f0314538536eadd8f77f0931d7cf9b556a961
Ruixin <tsuzsrx@gmail.com>
2021-05-23 13:48:25 -0230

    Exercise 1.27: reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'rt')
+header = next(f).split(',')
+
+cost = 0.0
+for line in f:
+    row = line.split(',')
+    shares = int(row[1])
+    price = float(row[2])
+    cost += shares * price
+
+print('Total cost', cost)
+
+f.close()
