a871c3a08a9ad2117279c489bb17959c3d42136e
Ruixin <tsuzsrx@gmail.com>
2021-05-24 13:32:57 -0230

    Exercise 1.33: comments added

--- Work/pcost.py
+++ Work/pcost.py
@@ -7,21 +7,23 @@ import sys
 def portfolio_cost(filename):
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
-        header = next(rows)
+        # skipping the headline
+        next(rows)
         cost = 0.0
         for row in rows:
             try:
                 shares = int(row[1])
                 price = float(row[2])
             except ValueError:
-                print("This line missing something, couldn't parse:", row)
+                print("This line missing something, couldn't be parsed:", row)
             cost += shares * price
     return cost
 
+# reading filename from command line
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
     filename = 'Data/portfolio.csv'
 
 cost = portfolio_cost(filename)
-print('Total cost', cost)
+print(f'Total cost: {cost:>.2f}')
