52e4ebd57f6a01ec7af3e2e6449f07e4d49a80d2
Ruixin <tsuzsrx@gmail.com>
2021-05-26 21:51:20 -0230

    Exercise 2.7: Finding gain/loss

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.6
+# Exercise 2.7
 import csv
 
 def read_portfolio(filename):
@@ -24,7 +24,24 @@ def read_prices(filename):
         for row in rows:
             try:
                 prices[row[0]] = float(row[1])
-                print("Successfully added to dict.")
             except:
                 print("Empty line, can't add to dict!")
     return prices
+
+def gain_loss():
+    portfolio = read_portfolio('Data/portfolio.csv')
+    prices = read_prices('Data/prices.csv')
+    total = 0.0
+    for s in portfolio:
+        if s['name'] in prices:
+            current_price = prices[s['name']]
+            buy_price = s['price']
+            profit = (current_price - buy_price) * s['shares']
+            if profit > 0:
+                print(f'You gain {profit: 0.2f} in the stock', s['name'], ', you have been closer to retire! :D')
+            else:
+                print(f'You lost {profit: 0.2f} in the stock', s['name'])
+            total += profit
+    print(f'Your total gain/loss is {total: 0.2f}')
+
+gain_loss()
