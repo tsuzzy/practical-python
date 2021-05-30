8b0a135a1be6f3af87bba74731939af14bcaa7dd
Ruixin <tsuzsrx@gmail.com>
2021-05-30 18:42:04 -0230

    Exercise 3.6: Working without headers

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,41 +1,39 @@
 # fileparse.py
 #
-# Exercise 3.5
+# Exercise 3.6
 import csv
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=True):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f)
-
-        # Read the file headers
-        headers = next(rows)
-
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
-
         records = []
+
+        if has_headers:
+            headers = next(rows)
+            if select: # If a column selector was given, find indices of the specified columns.
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
+        
         for row in rows:
-            if not row:    # Skip rows with no data
+            if not row:  # Skip rows without data
                 continue
-
-            # Filter the row if specific columns were selected
-            if indices:
-                row = [ row[index] for index in indices ]
-
-            # Map each element in row with a data type in the list "types"
-            if types:
+            
+            if types: # Map each element in row with a data type in the list "types"
                 row = [func(val) for func, val in zip(types, row)]
             
-            # Make a dict
-            record = dict(zip(headers, row))
+            if has_headers: # With headers, making a dictionary for records
+                if indices: # Filter the row if specific columns were selected
+                    row = [ row[index] for index in indices ]
+                record = dict(zip(headers,row))
+            else: # Without headers, using tuples as substitute
+                record = [item for item in row]
+                record = tuple(record)
+            
             records.append(record)
 
     return records
