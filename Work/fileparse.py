470455f9e559f6dbf9b658daf3df42b851677952
Ruixin <tsuzsrx@gmail.com>
2021-05-30 21:51:45 -0230

    Exercise 3.8: Raising exception

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,6 +1,6 @@
 # fileparse.py
 #
-# Exercise 3.7
+# Exercise 3.8
 import csv
 
 def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=None):
@@ -14,13 +14,19 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=Non
             rows = csv.reader(f)
         records = []
 
+        # I did not figure out how Raise works, 
+        # so this piece of code(line 19 and 20) are refered from ./Solution
+        if select and not has_headers:
+            raise RuntimeError('Select argument requires column headers')
+
         if has_headers:
             headers = next(rows)
-            if select: # If a column selector was given, find indices of the specified columns.
-                indices = [headers.index(colname) for colname in select]
-                headers = select
-            else:
-                indices = []
+        
+        if select: # If a column selector was given, find indices of the specified columns.
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
         
         for row in rows:
             if not row:  # Skip rows without data
